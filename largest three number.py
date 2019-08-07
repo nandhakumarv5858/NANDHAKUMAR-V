9be4c60f7b744c3f1d5b456cc538de0fa7396000
nandhakumarv5858 <46603021+nandhakumarv5858@users.noreply.github.com>
2019-08-07 14:57:23 +0530

    Create largest three number.py

---
+++ largest three number.py
@@ -0,0 +1,12 @@
+a = float(input("Enter first number"))
+b= float(input("Enter second number"))
+c= float(input("Enter third number"))
+ 
+if (a > b) and (b> c):
+   largest = num1
+elif (b> a) and (b> c):
+   largest = b
+else:
+   largest = c
+   print(largest)
+ 
